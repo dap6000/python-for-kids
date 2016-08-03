3682eeee909479fdbeac5abca3e95e9680656fed
dap6000 <dap6000@gmail.com>
2016-08-03 16:34:14 -0500

    Chapter 1

---
+++ Chapter 1/facts.py
@@ -0,0 +1,15 @@
+# yes this is getting a bit ahead of ourselves. we're using a variable (from
+# chapter 2) and a list (from chapter 3) of strings (also from chapter 3). but
+# i think it is a handy way to approach fact gathering for this project. and we
+# may be able to do something cool with it later.
+chapter1Facts = [
+    "Computer hardware refers to its physical parts: wires, microchips, hard drives, etc.",
+    "A computer program is a set of instructions that tell the computer to do something.",
+    "Software is a collection of computer programs.",
+    "A programming language lets a person write instructions in a way a computer can understand.",
+    "The Python programming language is named after the TV show Monty Pythyon's Flying Circus",
+    "The print() command is an example of a function.",
+    "The stuff between the () characters in a function is called a parameter.",
+    "Functions can have zero, one, or many paramters.",
+    "The print() function displays its paramter on the screen"
+]
---
+++ Chapter 1/hello.py
@@ -0,0 +1,3 @@
+# personally i think hello world examples are pretty useless but this book
+# follows the classic convention and opens with it. so here we are.
+print("Hello World")
