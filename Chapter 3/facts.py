ef553cdab1af1c86d8f53adc04ee27b8c4488f8c
dap6000 <dap6000@gmail.com>
2016-08-11 16:22:34 -0500

    Starting chapter 3

---
+++ Chapter 3/facts.py
@@ -0,0 +1,14 @@
+chapter3Facts = [
+    "Strings give us a way to set apart text to be treated as a value rather than as code",
+    "Python needs to know where a string starts and where it ends, so it can start treating text as code again",
+    "We mark the beginning and end of a string with quote marks",
+    "Strings can be quoted using the single quote character ('), the double quote character(\"), or three single quote characters (''')",
+    "The style of quote used at the end of a string MUST match the style of quote used at the start of that string",
+    "Syntax is the rules for how words and symbols are used in a language. English has syntax rules. Python has them too.",
+    "A SyntaxError means something in the code broke the rules of syntax for Python",
+    "Sometimes error messages can contain cryptic jargon like EOL. EOL is an abbreviation for End Of Line."
+    "The triple quote style let's us use line breaks inside our strings. That will cause an end of line error with the other two quote styles.",
+    "If you want to use the same character inside a string that is used to mark the start and end of that string then you have to escape the character used inside the string with a backslash character (\)",
+    '''The triple quote syntax for strings let's us use single quotes (') and double quotes (") in our strings without having to escape them.''',
+    
+]
